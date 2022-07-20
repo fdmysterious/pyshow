d262ffccc1af58a5618227253b5d2b05da6b9097
Florian Dupeyron <florian.dupeyron@mugcat.fr>
2022-07-20 18:23:32 +0200

    Add callback system to midi controller

--- src/pyshow/midi/control/__init__.py
+++ src/pyshow/midi/control/__init__.py
@@ -21,7 +21,26 @@ class Control_Desk_MIDI(Control_Desk):
         super().__init__()
         self._in_port   = rtmidi.MidiIn()
         self._port_name = port_name
+
+        self._filters   = dict()
+
+    def event_register(self, template_msg: midi.Midi_Msg, cbk):
+        ch_num = template_msg.chan
+        ttype  = template_msg.type
+        values = template_msg.values
+
+        if not ch_num in self._filters:
+            self._filters[ch_num] = dict()
+
+        if not ttype in self._filters[ch_num]:
+            self._filters[ch_num][ttype] = dict()
+
+        if values[0] in self._filters[ch_num][ttype]:
+            raise ValueError(f"An event is already registered for event {ev}")
+        else:
+            self._filters[ch_num][ttype][values[0]] = cbk
         
+
     async def _hook_open(self):
         self._in_port.open_virtual_port(self._port_name)
         self._in_port.set_callback(self._callback)
@@ -31,6 +50,13 @@ class Control_Desk_MIDI(Control_Desk):
 
     async def _hook_process(self, ev):
         print(f"Received MIDI: {ev}")
+
+        # FIXME # Kinda ugly
+        if ev.chan in self._filters:
+            if ev.type in self._filters[ev.chan]:
+                if ev.values[0] in self._filters[ev.chan][ev.type]:
+                    self._filters[ev.chan][ev.type][ev.values[0]](ev)
+
 
     def _callback(self, ev, data=None):
         msg,deltaT = ev
--- src/test_midi_control.py
+++ src/test_midi_control.py
@@ -9,10 +9,14 @@
 
 import asyncio
 from pyshow.midi.control import Control_Desk_MIDI
+from pyshow.midi         import codec as midi
 
 async def main():
     loop = asyncio.get_running_loop()
     ctrl = Control_Desk_MIDI()
+
+    ctrl.event_register(midi.note_on(0, 32, 0), lambda ev: print("Special ev!"))
+    ctrl.event_register(midi.cc     (0, 1 , 0), lambda ev: print(f"Modulation! {ev.values[1]}"))
 
     ctrl.start(loop)
     try:
